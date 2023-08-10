f1d81fa3f27ac3fa8ad22271d4ad71d5c8c6607d
Romyhoyos <hoyosandrea977>
2023-08-10 11:39:38 -0300

    new

--- graficos.py
+++ graficos.py
@@ -6,10 +6,8 @@ raiz.title("Eventos Musicales")
 
 raiz.resizable(True,False)
 
-raiz.iconbitmap("img/musica.ico")
-
 raiz.geometry("650x350")
 
-raiz.config(bg="9F93A3")
+raiz.config(bg="rosa pálido")
 
 raiz.mainloop()
