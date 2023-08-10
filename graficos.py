47c6901ce392be33d0ba01b7c98d77614b657d6b
Romyhoyos <hoyosandrea977>
2023-08-10 11:03:13 -0300

    First

---
+++ graficos.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+raiz=Tk()
+
+raiz.title("Eventos Musicales")
+
+raiz.resizable(True,False)
+
+raiz.iconbitmap("img/musica.ico")
+
+raiz.geometry("650x350")
+
+raiz.mainloop()
